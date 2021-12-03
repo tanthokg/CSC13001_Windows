b0fbb5b031006c7fbd83cf1ce50829727a32f469
tanthokg <htho379@gmail.com>
2021-12-03 20:38:00 +0700

    Chosen rules item listview
    - Display a list of chosen rules in listview
    - Add and remove a rule to list
    - Move chosen item around in the list

--- BatchRename/BatchRename/MainWindow.xaml.cs
+++ BatchRename/BatchRename/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,9 @@ namespace BatchRename
     /// </summary>
     public partial class MainWindow : Fluent.RibbonWindow
     {
+        private BindingList<string> rules;
+        private BindingList<string> chosenRules;
+        private int methodCounter = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +31,80 @@ namespace BatchRename
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
+            rules = new BindingList<string>() {
+                "Replace", "UPPERCASE", "lowercase", "PascalCase", "Add Prefix", "Add Suffix"
+            };
+            chosenRules = new BindingList<string>();
+            methodComboxBox.ItemsSource = rules;
+            chosenListView.ItemsSource = chosenRules;
+        }
 
+        private void AddRules(object sender, RoutedEventArgs e)
+        {
+            int index = methodComboxBox.SelectedIndex;
+            if (index != -1)
+            {
+                methodCounter++;
+                chosenRules.Add(rules[index]);
+            }
+        }
+
+        private void RemoveChosenFromList(object sender, RoutedEventArgs e)
+        {
+            int index = chosenListView.SelectedIndex;
+            if (index != -1)
+                chosenRules.RemoveAt(index);
+        }
+
+        private void ResetChosenRules(object sender, RoutedEventArgs e)
+        {
+            chosenRules.Clear();
+        }
+
+        private void MoveRuleToTop(object sender, RoutedEventArgs e)
+        {
+            int index = chosenListView.SelectedIndex;
+            if (index != -1)
+            {
+                string temp = chosenRules[index];
+                for (int i = index; i > 0; --i)
+                    chosenRules[i] = chosenRules[i - 1];
+                chosenRules[0] = temp;
+            }
+        }
+
+        private void MoveRuleToPrev(object sender, RoutedEventArgs e)
+        {
+            int index = chosenListView.SelectedIndex;
+            if (index != -1 && index != 0)
+            {
+                string temp = chosenRules[index - 1];
+                chosenRules[index - 1] = chosenRules[index];
+                chosenRules[index] = temp;
+            }
+        }
+
+        private void MoveRuleToNext(object sender, RoutedEventArgs e)
+        {
+            int index = chosenListView.SelectedIndex;
+            if (index != -1 && index != chosenRules.Count - 1)
+            {
+                string temp = chosenRules[index + 1];
+                chosenRules[index + 1] = chosenRules[index];
+                chosenRules[index] = temp;
+            }
+        }
+
+        private void MoveRuleToLow(object sender, RoutedEventArgs e)
+        {
+            int index = chosenListView.SelectedIndex;
+            if (index != -1)
+            {
+                string temp = chosenRules[index];
+                for (int i = index; i < chosenRules.Count - 1; ++i)
+                    chosenRules[i] = chosenRules[i + 1];
+                chosenRules[chosenRules.Count - 1] = temp;
+            }
         }
     }
 }
